5d71a6ebc1a6d446600303b32cb06036a075c53a
Samuel Hinton <samuelreay@gmail.com>
2016-07-19 12:01:49 +1000

    Adding travis and deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ];
+    then exit 0;
+fi
+if [ -n "$GITHUB_API_KEY" ]; then
+    echo "Github key found. Building documentation."
+    cd "$TRAVIS_BUILD_DIR"/doc
+    make clean
+    make html
+    make html
+    cd "$TRAVIS_BUILD_DIR"
+    rm -rf .git/
+    cd doc/out/html
+    git config --global user.email "travis"
+    git config --global user.name "travis"
+    touch .nojekll
+    git init
+    git add .
+    echo "Committing"
+    git commit -m init
+    # Make sure to make the output quiet, or else the API token will leak!
+    # This works because the API key can replace your password.
+    echo "Pushing"
+    git push -f -q "https://${GITHUB_API_KEY2}@${GH_REF}" master:gh-pages > /dev/null 2>&1 && echo "Pushed"
+fi
+echo "Deploy script ending"
