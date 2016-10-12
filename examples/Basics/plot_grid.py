9bb6817c83a8d215fdb2de8552cf07230514b2e2
Samuel Hinton <samuelreay@gmail.com>
2016-10-12 14:49:24 +1000

    Updating code example for griddata

--- examples/Basics/plot_grid.py
+++ examples/Basics/plot_grid.py
@@ -5,9 +5,8 @@ Grid Data!
 
 If you don't have Monte Carlo chains, and have grid evaluations instead, that's fine too!
 
-Just flatten your grid, set the weights to the grid evaluation, and set the grid flag.
-
-Here is a nice diamond that you get from modifying a simple multivariate normal distribution.
+Just flatten your grid, set the weights to the grid evaluation, and set the grid flag. Here is
+a nice diamond that you get from modifying a simple multivariate normal distribution.
 
 """
 import numpy as np
@@ -21,7 +20,6 @@ if __name__ == "__main__":
     data = np.vstack((xs, ys)).T
     pdf = (1 / (2 * np.pi)) * np.exp(-0.5 * (xs * xs + ys * ys / 4 + np.abs(xs * ys)))
 
-    c = ChainConsumer()
     c = ChainConsumer()
     c.add_chain(data, parameters=["$x$", "$y$"], weights=pdf, grid=True)
     fig = c.plot()
