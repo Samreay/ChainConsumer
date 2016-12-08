a721ad20181211389a021751f2a5c58836d795d2
Samuel Hinton <samuelreay@gmail.com>
2016-12-08 12:46:26 +0000

    Updating an example plot

--- examples/customisations/plot_lists.py
+++ examples/customisations/plot_lists.py
@@ -21,17 +21,17 @@ if __name__ == "__main__":
     cov = normal(size=(2, 2)) + np.identity(2)
     d2 = multivariate_normal(normal(size=2), 0.5 * (cov + cov.T), size=100000)
     cov = normal(size=(2, 2)) + np.identity(2)
-    d3 = multivariate_normal(normal(size=2), 0.5 * (cov + cov.T), size=100000)
+    d3 = multivariate_normal(normal(size=2), 0.5 * (cov + cov.T), size=1000000)
 
     c = ChainConsumer()
     c.add_chain(d1, parameters=["$x$", "$y$"])
     c.add_chain(d2)
     c.add_chain(d3)
 
-    c.configure_general(linestyles=["-", "--", ":"], linewidths=[1.0, 2.0, 2.5], bins=[3.0, 1.0, 1.0],
-                        colours=["#1E88E5", "#D32F2F", "#111111"], smooth=[0,1,2])
+    c.configure_general(linestyles=["-", "--", "-"], linewidths=[1.0, 3.0, 1.0], bins=[3.0, 1.0, 1.0],
+                        colours=["#1E88E5", "#D32F2F", "#111111"], smooth=[0, 1, 2])
     c.configure_contour(shade=[True, True, False], shade_alpha=[0.2, 0.1, 0.0])
     c.configure_bar(shade=[True, False, False])
     fig = c.plot()
 
-    fig.set_size_inches(2.5 + fig.get_size_inches())  # Resize fig for doco. You don't need this.
+    fig.set_size_inches(3 + fig.get_size_inches())  # Resize fig for doco. You don't need this.
