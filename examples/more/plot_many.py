fca5cc506cb13adf1964ebec2f4e3216dc2a2fbc
Samuel Hinton <samuelreay@gmail.com>
2016-12-09 10:41:41 +0000

    Reformating file

--- examples/more/plot_many.py
+++ examples/more/plot_many.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     np.random.seed(1)
     n = 100000
     data = multivariate_normal([0.4, 1], [[0.01, -0.003], [-0.003, 0.001]], size=n)
-    data = np.hstack((data, (67 + 10 * data[:, 0] - data[:, 1]**2)[:,None]))
+    data = np.hstack((data, (67 + 10 * data[:, 0] - data[:, 1] ** 2)[:, None]))
     data2 = np.vstack((uniform(-0.1, 1.1, n), normal(1.2, 0.1, n))).T
-    data2[:, 1] -= (data2[:,0] ** 2)
+    data2[:, 1] -= (data2[:, 0] ** 2)
     data3 = multivariate_normal([0.3, 0.7], [[0.02, 0.05], [0.05, 0.1]], size=n)
 
     c = ChainConsumer()
@@ -31,6 +31,6 @@ if __name__ == "__main__":
 
     c.configure(color_params="$H_0$", shade=[True, True, False],
                 shade_alpha=0.2, bar_shade=True, linestyles=["-", "--", "-"])
-    fig = c.plot(figsize=2.0)
+    fig = c.plot(figsize=2.0, extents=[[0, 1], [0, 1.5]])
 
     fig.set_size_inches(3.0 + fig.get_size_inches())  # Resize fig for doco. You don't need this.
