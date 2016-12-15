a509fe288483174cdb552f9b3cdea4b2cfee4326
Samuel Hinton <samuelreay@gmail.com>
2016-12-15 12:14:08 +0000

    Updating tolerances

--- test_chain.py
+++ test_chain.py
@@ -706,7 +706,7 @@ class TestChain(object):
         assert len(aics) == 2
         assert aics[0] == 0
         expected = 2 * np.log(2)
-        assert np.isclose(aics[1], expected)
+        assert np.isclose(aics[1], expected, atol=1e-3)
 
     def test_aic_parameter_dependence(self):
         d = norm.rvs(size=1000)
@@ -718,7 +718,7 @@ class TestChain(object):
         assert len(aics) == 2
         assert aics[0] == 0
         expected = 1 * 2 + (2.0 * 2 * 3 / (1000 - 2 - 1)) - (2.0 * 1 * 2 / (1000 - 1 - 1))
-        assert np.isclose(aics[1], expected)
+        assert np.isclose(aics[1], expected, atol=1e-3)
 
     def test_aic_data_dependence(self):
         d = norm.rvs(size=1000)
@@ -730,7 +730,7 @@ class TestChain(object):
         assert len(aics) == 2
         assert aics[0] == 0
         expected = (2.0 * 1 * 2 / (500 - 1 - 1)) - (2.0 * 1 * 2 / (1000 - 1 - 1))
-        assert np.isclose(aics[1], expected)
+        assert np.isclose(aics[1], expected, atol=1e-3)
 
     def test_bic_fail_no_posterior(self):
         d = norm.rvs(size=1000)
@@ -778,7 +778,7 @@ class TestChain(object):
         assert len(bics) == 2
         assert bics[0] == 0
         expected = 2 * np.log(2)
-        assert np.isclose(bics[1], expected)
+        assert np.isclose(bics[1], expected, atol=1e-3)
 
     def test_bic_parameter_dependence(self):
         d = norm.rvs(size=1000)
@@ -790,7 +790,7 @@ class TestChain(object):
         assert len(bics) == 2
         assert bics[0] == 0
         expected = np.log(1000)
-        assert np.isclose(bics[1], expected)
+        assert np.isclose(bics[1], expected, atol=1e-3)
 
     def test_bic_data_dependence(self):
         d = norm.rvs(size=1000)
@@ -802,7 +802,7 @@ class TestChain(object):
         assert len(bics) == 2
         assert bics[1] == 0
         expected = np.log(1000) - np.log(500)
-        assert np.isclose(bics[0], expected)
+        assert np.isclose(bics[0], expected, atol=1e-3)
 
     def test_bic_data_dependence2(self):
         d = norm.rvs(size=1000)
@@ -814,7 +814,7 @@ class TestChain(object):
         assert len(bics) == 2
         assert bics[0] == 0
         expected = 3 * np.log(500) - 2 * np.log(1000)
-        assert np.isclose(bics[1], expected)
+        assert np.isclose(bics[1], expected, atol=1e-3)
 
     def test_dic_fail_no_posterior(self):
         d = norm.rvs(size=1000)
@@ -845,5 +845,5 @@ class TestChain(object):
         assert bics[1] == 0
         dic1 = 2 * np.mean(-2 * p) + 2 * norm.logpdf(0)
         dic2 = 2 * np.mean(-2 * p2) + 2 * norm.logpdf(0, scale=2)
-        assert np.isclose(bics[0], dic1 - dic2)
+        assert np.isclose(bics[0], dic1 - dic2, atol=1e-3)
 
