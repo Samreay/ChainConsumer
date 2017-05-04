a159a76c3a9fa44b03ed28b8f84e486673b053eb
Samuel Hinton <samuelreay@gmail.com>
2017-05-04 11:24:30 +1000

    Updating one chain example

--- examples/customisations/plot_one_chain.py
+++ examples/customisations/plot_one_chain.py
@@ -17,8 +17,8 @@ from chainconsumer import ChainConsumer
 
 if __name__ == "__main__":
     np.random.seed(0)
-    cov = 1e5 * normal(size=(3, 3))
-    data = multivariate_normal(1e8 * normal(size=3), 0.5 * (cov + cov.T), size=100000)
+    cov = 1e2 * normal(size=(3, 3))
+    data = multivariate_normal(1e3 * normal(size=3), 0.5 * (cov + cov.T), size=100000)
 
     # If you pass in parameter labels and only one chain, you can also get parameter bounds
     fig = ChainConsumer().add_chain(data, parameters=["$x$", "$y$", r"$\epsilon$"]).plot()
