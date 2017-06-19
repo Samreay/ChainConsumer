Fixing bug in covariance!

test_chain.py
@@ -1190,15 +1190,16 @@ class TestChain(object):
         assert np.isclose(maxv, 3, atol=0.001)
 
     def test_covariant_covariance_calc(self):
-        data1 = np.random.multivariate_normal([0, 0], [[1, 0], [0, 1]], size=1000)
-        data2 = np.random.multivariate_normal([0, 0], [[1, 1], [1, 1]], size=1000)
-        weights = np.concatenate((np.ones(1000), np.zeros(1000)))
+        data1 = np.random.multivariate_normal([0, 0], [[1, 0], [0, 1]], size=10000)
+        data2 = np.random.multivariate_normal([0, 0], [[2, 1], [1, 2]], size=10000)
+        weights = np.concatenate((np.ones(10000), np.zeros(10000)))
         data = np.concatenate((data1, data2))
         c = ChainConsumer()
-        c.add_chain(data, weights=weights, parameters=["x"])
+        c.add_chain(data, weights=weights, parameters=["x", "y"])
         p, cor = c.analysis.get_covariance()
         assert p[0] == "x"
-        assert np.isclose(cor[0, 0], 1, atol=1e-2)
-        assert np.isclose(cor[1, 1], 1, atol=1e-2)
-        assert np.isclose(cor[0, 1], 0, atol=1e-2)
+        assert p[1] == "y"
+        assert np.isclose(cor[0, 0], 1, atol=2e-2)
+        assert np.isclose(cor[1, 1], 1, atol=2e-2)
+        assert np.isclose(cor[0, 1], 0, atol=2e-2)
         assert cor.shape == (2, 2)