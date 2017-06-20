9a3741dc58edce54c79acada9eb363505c0ccd1b
Samuel Hinton <samuelreay@gmail.com>
2017-06-20 12:53:05 -0700

    Updating KDE example

--- examples/customisations/plot_kde_extents.py
+++ examples/customisations/plot_kde_extents.py
@@ -9,20 +9,21 @@ Smooth marginalised distributions with a Gaussian KDE, and pick custom extents.
 Note that invoking the KDE on large data sets will significantly increase rendering time when
 you have a large number of points.
 
-Also note that if you pass a floating point number to bins, it multiplies the default bin size
-(which is a function of number of steps in the chain) by that amount. If you give it an integer,
-it will use that many bins.
+You can see the increase in contour smoothness (without broadening) for when you have a
+low number of samples in your chains!
 """
 
 import numpy as np
 from chainconsumer import ChainConsumer
 
 if __name__ == "__main__":
-    data = np.random.multivariate_normal([0.0, 4.0], [[1.0, 0.7], [0.7, 1.5]], size=50000)
+    np.random.seed(0)
+    data = np.random.multivariate_normal([0.0, 4.0], [[1.0, -0.7], [-0.7, 1.5]], size=3000)
 
     c = ChainConsumer()
-    c.add_chain(data)
-    c.configure(bins=0.9, kde=True)
-    fig = c.plotter.plot(extents=[(-2, 4), (0, 10)])
+    c.add_chain(data, name="KDE on")
+    c.add_chain(data + 1, name="KDE off")
+    c.configure(kde=[True, False], shade_alpha=0.1, flip=False)
+    fig = c.plotter.plot(extents=[(-2, 3), (0, 8)])
 
     fig.set_size_inches(4.5 + fig.get_size_inches())  # Resize fig for doco. You don't need this.
