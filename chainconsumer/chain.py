43f90ad954327512b8f3ae2ba65a1f2fe1f95007
Samuel Hinton <samuelreay@gmail.com>
2017-12-12 23:30:14 +1100

    v0.23.2

--- chainconsumer/chain.py
+++ chainconsumer/chain.py
@@ -17,7 +17,7 @@ class ChainConsumer(object):
     figures, tables, diagnostics, you name it.
 
     """
-    __version__ = "0.23.1"
+    __version__ = "0.23.2"
 
     def __init__(self):
         logging.basicConfig()
