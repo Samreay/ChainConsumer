89be7ae643743969b592821ffe47f659415a4fc5
Casey McGrath <caseymcgrath77@yahoo.com>
2025-03-13 16:43:14 -0400

    Add new plotting style choices for the truth markers.  If the user specifies the arguments 'off_diagonal_marker' and 'off_diagonal_marker_size' in the declaration of the 'Truth' object, then use that information to place specific markers at the truth locations in the bottom diagonal.  Default is to use the original style for the truth markers, which are just vertical/horizontal lines across eeach of the subplots in the corner.

--- src/chainconsumer/plotting/truth.py
+++ src/chainconsumer/plotting/truth.py
@@ -6,11 +6,27 @@ from ..truth import Truth
 
 def plot_truths(ax: Axes, truths: list[Truth], px: ColumnName | None = None, py: ColumnName | None = None) -> None:
     for truth in truths:
-        if px is not None:
-            val_x = truth.location.get(px)
-            if val_x is not None:
-                ax.axvline(val_x, **truth._kwargs)
-        if py is not None:
-            val_y = truth.location.get(py)
-            if val_y is not None:
-                ax.axhline(val_y, **truth._kwargs)
+        # DEFAULT PLOTTING STYLE
+        if truth.off_diagonal_marker is None:
+            for truth in truths:
+                if px is not None:
+                    val_x = truth.location.get(px)
+                    if val_x is not None:
+                        ax.axvline(val_x, **truth._kwargs)
+                if py is not None:
+                    val_y = truth.location.get(py)
+                    if val_y is not None:
+                        ax.axhline(val_y, **truth._kwargs)
+        # ALTERNATE PLOTTING STYLE
+        if truth.off_diagonal_marker:
+            if px is not None:
+                # Plot vertical line ONLY along diagonal
+                if py is None:
+                    val_x = truth.location.get(px)
+                    if val_x is not None:
+                        ax.axvline(val_x, **truth._kwargs)
+            # Plot specified markers on the off-diagonals
+            if py is not None:
+                val_x = truth.location.get(px)
+                val_y = truth.location.get(py)
+                ax.scatter(val_x, val_y, marker=truth.off_diagonal_marker, s=truth.off_diagonal_marker_size, c=truth._kwargs['c'], alpha=truth._kwargs['alpha'], zorder=truth._kwargs['zorder'])
